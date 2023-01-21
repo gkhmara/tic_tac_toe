58d4afa54cc1feefed3bece146b00533721a2749
Greg Khmara <gkhmara@gmail.com>
2023-01-21 13:31:12 -0800

    create code for game and player names

---
+++ src/index.js
@@ -0,0 +1,76 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+class Square extends React.Component {
+  render() {
+    return <button className="square">{/* TODO */}</button>;
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = "Next player: X";
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class PlayerNames extends React.Component {
+  render() {
+    return (
+      <div className="player-names">
+        <h1>Names of Players</h1>
+        <ul>
+          <li>Player One: {this.props.playerOne}</li>
+          <li>Player Two: {this.props.playerTwo}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+          <PlayerNames playerOne="Greg" playerTwo="John" />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// =======================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
